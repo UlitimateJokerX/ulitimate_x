1775a1d60514615718b68baf04f21e52b4fe891d
Cullen <cullen@example.com>
2024-11-28 14:13:48 +0800

    Rename title

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -29,7 +29,7 @@ function MainNavigation (props) {
     return (
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
-          <Navbar.Brand href='/'>Ultimate X</Navbar.Brand>
+          <Navbar.Brand href='/'>脆皮雪糕</Navbar.Brand>
         </Container>
       </Navbar>
     )
@@ -38,7 +38,7 @@ function MainNavigation (props) {
   return (
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container fluid>
-        <Navbar.Brand href='/'>Ultimate X</Navbar.Brand>
+        <Navbar.Brand href='/'>脆皮雪糕</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
