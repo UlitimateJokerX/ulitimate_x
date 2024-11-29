68c7244ff982cf1d7deb98a2d85b562166ce056f
Cullen <cullen@example.com>
2024-11-29 14:02:30 +0800

    Add delete message button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Table from 'react-bootstrap/Table'
-import { Form, Row, Col } from 'react-bootstrap'
+import { Form, Row, Col, Button } from 'react-bootstrap'
 import { Tab, Tabs } from 'react-bootstrap'
 import Badge from 'react-bootstrap/Badge'
 import Notification, { notify } from 'react-notify-bootstrap'
@@ -37,6 +37,35 @@ async function handleSend (e, username, getMsgList, funcs) {
     })
 }
 
+async function handleDel (e, msgId, getMsgList, funcs) {
+  const answer = window.confirm('Are you sure you want to delete this message?')
+
+  if (answer) {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        message_id: msgId,
+        new_data: {deleted: 1}
+      })
+    }
+
+    await fetch('/api/message', requestOptions)
+    .then(r => r.json())
+    .then(d => {
+      if (d.result !== 'ok') {
+        throw new Error(d.msg)
+      }
+
+      // reload
+      getMsgList(funcs)
+    })
+    .catch(e => {
+      alert(`Call omnipotent system error: ${e.message}`)
+    })
+  }
+}
+
 // 撈取訊息
 async function getMsgList (funcs) {
   await fetch('/api/message')
@@ -55,17 +84,39 @@ async function getMsgList (funcs) {
 // 列出訊息列表
 function ShowMsgList (props) {
   const msgList = props.msgList
+  const loginUser = props.username
+  const getMsgList = props.getMsgList
+  const funcs = props.funcs
 
   return (
     <>
       <h1 />
       {
         msgList.map((msg, index) => {
+          if (msg.deleted == 1) {
+            return (
+              <div key={index}>
+              </div>
+            )
+          }
+
           return (
             <div key={index}>
               <Col sm='12'>
                 <Badge pill bg={msg.operator.toLowerCase() == 'jenny' ? 'info' : 'dark'}>{msg.operator}</Badge>
-                {' '}{msg.created_at}
+                {' '}{msg.created_at}{' '}
+
+                {
+                  loginUser.toLowerCase() == msg.operator.toLowerCase() ?
+                  <button className="btn btn-outline-secondary border-0" type="button" onClick={e => handleDel(e, msg.id, getMsgList, funcs)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                      <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                  </button>
+                  :
+                  <></>
+                }
                 <br />
                 {
                   <pre>{msg.content}</pre>
@@ -113,7 +164,7 @@ function HomePage (props) {
         <Form.Group className='mb-12' as={Row}>
           <Col sm='3' />
           <Col sm='6'>
-            <ShowMsgList msgList={msgList} />
+            <ShowMsgList msgList={msgList} username={username} getMsgList={getMsgList} funcs={{setMsgList}} />
           </Col>
           <Col sm='3' />
         </Form.Group>
